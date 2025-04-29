a563e0d0eeca97f985e3c2805b70d4ca4c5e888e
Sale2025 <sale.jelic@gmail.com>
2025-04-29 13:14:35 +0200

    useradminrolefinish

---
+++ templates/security/LoginSuccessHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    private RouterInterface $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
+    {
+        $roles = $token->getRoleNames();
+
+        if (in_array('ROLE_ADMIN', $roles, true)) {
+            return new RedirectResponse($this->router->generate('admin_dashboard'));
+        }
+
+        return new RedirectResponse($this->router->generate('home'));
+    }
+}
